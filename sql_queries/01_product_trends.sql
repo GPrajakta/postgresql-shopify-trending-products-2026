2de383931f3aa26298c7a42b70bf93b208648ba8
GPrajakta <parijatak43@gmail.com>
2026-03-13 14:54:48 +0530

    Update 01_product_trends.sql

--- sql_queries/01_product_trends.sql
+++ sql_queries/01_product_trends.sql
@@ -4,3 +4,22 @@ SELECT product_name,category,trend_score
 FROM shopify_products
 ORDER BY trend_score DESC
 LIMIT 10;
+
+--Top 10 Products by Revenue--
+
+SELECT product_name,
+estimated_revenue_in_2025_usd
+FROM shopify_products
+ORDER BY estimated_revenue_in_2025_usd DESC
+LIMIT 10;
+
+--Products with High Trend but Low Revenue--
+
+SELECT product_name,
+trend_score,
+estimated_revenue_in_2025_usd
+FROM shopify_products
+WHERE trend_score > 8
+AND estimated_revenue_in_2025_usd < 50000;
+
+--
