7780bfe09f18449357fa1abe4753192e2034815e
GPrajakta <parijatak43@gmail.com>
2026-03-16 11:50:13 +0530

    Update 02_category_analysis.sql

--- sql_queries/02_category_analysis.sql
+++ sql_queries/02_category_analysis.sql
@@ -1,5 +1,14 @@
+---Category Revenue Analysis---
 SELECT category,
 SUM(estimated_revenue_in_2025_usd) AS revenue
 FROM shopify_products
 GROUP BY category
 ORDER BY revenue DESC;
+
+--Category Trend Score--
+select category,round(AVG(trend_score),2) AS avg_trend_score
+FROM shopify_trending_products_2026
+group by Category
+order by avg_trend_score DESC;
+
+---Category Contribution to Total Revenue---
